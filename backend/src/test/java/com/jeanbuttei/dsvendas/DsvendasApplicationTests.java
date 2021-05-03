3c878901cee7c50c1c827334b58137defdce057c
JeanButtei <jeanbuttei@gmail.com>
2021-05-03 09:32:53 -0300

    Project created

---
+++ backend/src/test/java/com/jeanbuttei/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanbuttei.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
