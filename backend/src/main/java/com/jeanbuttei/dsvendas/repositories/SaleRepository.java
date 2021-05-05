043c16743e7599c28579764b063f87ce770bfdbb
JeanButtei <jeanbuttei@gmail.com>
2021-05-05 09:40:56 -0300

    Group by search

--- backend/src/main/java/com/jeanbuttei/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/jeanbuttei/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.jeanbuttei.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.jeanbuttei.dsvendas.dto.SaleSuccessDTO;
+import com.jeanbuttei.dsvendas.dto.SaleSumDTO;
 import com.jeanbuttei.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
-
+	
+	@Query("SELECT new com.jeanbuttei.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.jeanbuttei.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/jeanbuttei/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/jeanbuttei/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.jeanbuttei.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jeanbuttei.dsvendas.dto.SaleDTO;
+import com.jeanbuttei.dsvendas.dto.SaleSuccessDTO;
+import com.jeanbuttei.dsvendas.dto.SaleSumDTO;
 import com.jeanbuttei.dsvendas.entities.Sale;
 import com.jeanbuttei.dsvendas.repositories.SaleRepository;
 import com.jeanbuttei.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll(); //Evitar muitas consultas para carregar Sellers
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
